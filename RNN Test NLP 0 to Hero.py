1ba7fa749772db7b4093a73604a57ae11a5eec4c
eezJerome <48054365+eezJerome@users.noreply.github.com>
2021-03-21 23:44:09 +1000

    Add files via upload

---
+++ RNN Test NLP 0 to Hero.py
@@ -0,0 +1,50 @@
+import tensorflow as tf
+
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.layers import Embedding, Dense, Bidirectional
+from tensorflow.compat.v1.keras.layers import CuDNNLSTM
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
+
+import numpy as np
+
+tokenizer = Tokenizer()
+
+data = open("Drake bot\data\songs.txt").read()
+
+corpus = data.lower().split("\n")
+
+tokenizer.fit_on_texts(corpus)
+total_words = len(tokenizer.word_index) + 1
+
+print(tokenizer.word_index)
+print(total_words)
+
+input_sequences = []
+for line in corpus:
+    token_list = tokenizer.texts_to_sequences([line])[0]
+    for i in range(1, len(token_list)):
+        n_gram_sequence = token_list[:i+1]
+        input_sequences.append(n_gram_sequence)
+
+# pad sequences
+max_sequence_len = max([len(x) for x in input_sequences])
+input_sequences = np.array(pad_sequences(input_sequences, maxlen=max_sequence_len, padding='pre'))
+
+# create predictions, labels
+xs, labels = input_sequences[:,:-1], input_sequences[:,-1]
+
+ys = tf.keras.utils.to_categorical(labels, num_classes=total_words)
+
+model = Sequential()
+model.add(Embedding(total_words,100, input_length=max_sequence_len))
+model.add(Bidirectional(CuDNNLSTM(150)))
+model.add(Dense(total_words, activation='softmax'))
+
+adam = Adam(lr=5e-4)
+
+model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
+
+history = model.fit(xs,ys, epochs=75, verbose=1)
+model.save(filepath='NLPtest1.h5',include_optimizer=True)
